bb7b2977c5030d014e3bc787c5f13e820429d674
Sahibjot Saggu <sahibjot@live.ca>
2017-02-02 12:38:05 -0500

    add user schema

---
+++ app/models/user.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+
+var UserSchema = new Schema({
+	name: String,
+	username: { 
+		type: String, 
+		required: true, 
+		index: { 
+			unique: true 
+		} 
+	},
+	password: {
+		type: String,
+		required: true,
+		select: false
+	}
+});
+
+// hash the password before saving it
+UserSchema.pre('save', function(next) {
+	var user = this;
+	if (!user.isModified('password')) {
+		return next();
+	}
+
+	bcrypt.hash(user.password, null, null, function(err, hash) {
+		if (err) {
+			return next(err);
+		}
+
+		user.password = hash;
+		next();
+	});
+});
+
+// method for comparing user entered password with hashed password in the database
+UserSchema.methods.comparePassword = function(password) {
+	var user = this;
+	bcrypt.compare(password, user.password, function(err, res) {
+		if (err) {
+			return err;
+		}
+		return res;
+	});
+};
+
+modoule.exports = mongoose.model('User', UserSchema);
